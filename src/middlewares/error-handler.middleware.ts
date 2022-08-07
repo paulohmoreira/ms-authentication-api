e26c5d92aa6a2bc5013b4fe9790b0ff62fd2e257
paulohmoreira <phmoreira37@gmail.com>
2022-08-07 13:28:32 -0300

    Criado conexão com banco de dados

---
+++ src/middlewares/error-handler.middleware.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import DatabaseError from "../models/errors/database.error.model";
+
+function errorHandler(error: any, request: Request, response: Response, next: NextFunction){
+    if (error instanceof DatabaseError) {
+        response.sendStatus(StatusCodes.BAD_REQUEST);
+    } else {
+        response.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
+export default errorHandler;
